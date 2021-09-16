bee9c01f01aa7abae4b5cc9f91f860018b5faa3e
welberthenrique <welberthenrique@gmail.com>
2021-09-16 14:15:46 -0300

    index, funções e validador para sessão

--- inc/database.php
+++ inc/database.php
@@ -19,3 +19,48 @@ function close_database($conn){
         echo $e->getMessage();
     }
 }
+
+
+function find($table = null, $id = null){
+
+    $database = open_database();
+    $found = null;
+
+    try{
+        id($id){
+            $sql = "SELECT * FROM " . $table . "WHERE id = " . $id;
+            $result = $database->query($sql);
+
+            if($result->num_rows > 0){
+                $found = $result->fetch_assoc();
+            }
+        }else{
+            $sql = "SELECT * FROM " . $table;
+            $result = $database->query($sql);
+
+            if($result->num_rows > 0){
+                $found = $result->fetch_all(MYSQLI_ASSOC);
+                
+                /*Metodo alternativo
+                $fount = array();
+
+                while ($row = $result->fetch_assoc()){
+                    array_push($found, $row);
+                }*/
+            }
+        }
+    }catch (Exception $e) {
+        $_SESSION['message'] = $e->getMessage();
+        $_SESSION['type'] = 'danger';
+    }
+    close_database($database);
+    return $found;
+}
+
+/**
+ * Pesquisa todos os registros de uma tabela
+ */
+
+ function find_all(){
+     return find($table);
+ }
